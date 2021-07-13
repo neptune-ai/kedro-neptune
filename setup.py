4a628f3402f13c350286268f188f3967cd1e6132
Rafal Jankowski <rafal.jankowski@neptune.ai>
2021-07-13 14:08:53 +0200

    client requirement bumped

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def main():
         all_deps += extras[group_name]
     extras['all'] = all_deps
 
-    base_libs = ['neptune-client>=0.10.1', 'kedro>=0.17.0']
+    base_libs = ['neptune-client>=0.10.2', 'kedro>=0.17.0']
 
     version = None
     if os.path.exists('PKG-INFO'):
