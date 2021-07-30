ba5d358f11704beb5861c8e8afca9276f01436cb
Rafal Jankowski <rafal.jankowski@neptune.ai>
2021-07-30 13:41:53 +0200

    Drop nonrequired and added Readme

---
+++ examples/planets/README.md
@@ -0,0 +1,9 @@
+# Kedro Neptune Integration Examples - Planets
+
+Initial Kedro project with two sample pipelines. It should be treated as a showcase of Kedro - Neptune integration.
+
+### Pipelines
+1. `Furthest Planet` - Simple DataFrame manipulations
+2. `Moons Classifier` - Training Machine Learning model with hyperparameters optimization.
+    - Optuna (integrated with Neptune.ai) for Bayesian optimization.
+    - Fast.ai integrated with Neptune.ai for training
--- examples/planets/src/setup.py
+++ examples/planets/src/setup.py
@@ -32,34 +32,20 @@ entry_point = (
     "planets = planets.__main__:main"
 )
 
-
-# get the dependencies and installs
-with open("requirements.txt", "r", encoding="utf-8") as f:
-    # Make sure we strip all comments and options (e.g "--extra-index-url")
-    # that arise from a modified pip.conf file that configure global options
-    # when running kedro build-reqs
-    requires = []
-    for line in f:
-        req = line.split("#", 1)[0].strip()
-        if req and not req.startswith("--"):
-            requires.append(req)
-
 setup(
     name="planets",
     version="0.1",
     packages=find_packages(exclude=["tests"]),
     entry_points={"console_scripts": [entry_point]},
-    install_requires=requires,
-    extras_require={
-        "docs": [
-            "sphinx~=3.4.3",
-            "sphinx_rtd_theme==0.5.1",
-            "nbsphinx==0.8.1",
-            "nbstripout~=0.4",
-            "recommonmark==0.7.1",
-            "sphinx-autodoc-typehints==1.11.1",
-            "sphinx_copybutton==0.3.1",
-            "ipykernel~=5.3",
-        ]
-    },
+    install_requires=[
+        'isort~=5.0',
+        'kedro==0.17.4',
+        'nbstripout~=0.4',
+        'pytest-cov~=2.5',
+        'pytest-mock>=1.7.1, <2.0',
+        'pytest~=6.2',
+        'wheel>=0.35, <0.37',
+        'kedro-neptune',
+        'neptune-client[optuna,fastai]>=0.10.2'
+    ],
 )
