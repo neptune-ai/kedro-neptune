c619fbc5d9f2059efc418f6a6981276fa60d9266
jakubczakon <czakon.jakub@gmail.com>
2021-08-20 08:54:37 +0200

    fixed formatting

--- kedro_neptune/__init__.py
+++ kedro_neptune/__init__.py
@@ -1,14 +1,14 @@
 #
 # Copyright (c) 2021, Neptune Labs Sp. z o.o.
 #
-# Licensed under the Apache License, Version 2.0 (the 'License');
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -388,7 +388,7 @@ class NeptuneHooks:
 
         for input_name, input_value in inputs.items():
             if input_name.startswith('params:'):
-                current_namespace[f'parameters/{input_name[len('params:'):]}'] = input_value
+                current_namespace[f'parameters/{input_name[len("params:"):]}'] = input_value
 
         self._node_execution_timers[node.short_name] = time.time()
 
