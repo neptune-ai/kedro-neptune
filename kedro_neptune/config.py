2345a28f59873347ab705d5f41b6774086c086c6
Orçun Gümüş <orcungumus@gmail.com>
2022-05-06 18:46:56 +0300

    enabled get default value of True
    enabled is an optional config so it needs to have a default value

--- kedro_neptune/config.py
+++ kedro_neptune/config.py
@@ -48,7 +48,7 @@ def get_neptune_config() -> NeptuneConfig:
     project = _parse_config_input(config['neptune']['project'])
     base_namespace = config['neptune']['base_namespace']
     source_files = config['neptune']['upload_source_files']
-    enabled = bool(_parse_config_input(config['neptune']))
+    enabled = bool(_parse_config_input(config['neptune'].get('enabled', True)))
 
     return NeptuneConfig(
         api_token=api_token,
