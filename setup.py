a4bd07179612867c4cab3da8fe18743f49f644f7
Rafal Jankowski <rafal.jankowski@neptune.ai>
2022-10-28 12:30:54 +0200

    Getting rid of pylint related annotations and configurations

--- setup.py
+++ setup.py
@@ -14,8 +14,6 @@ def main():
 
     extras = {
         "dev": [
-            "pylint==2.9.6",
-            "pylintfileheader",
             "pytest>=5.0",
             "pytest-cov==2.10.1",
             "neptune-client[optuna,fastai]>=0.15.2",
