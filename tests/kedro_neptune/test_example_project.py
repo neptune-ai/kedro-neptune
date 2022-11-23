141055b20732cf1524e0bd4e4796a257e03053a0
Rafal Jankowski <rafal.jankowski@neptune.ai>
2022-11-23 08:36:26 +0100

    Restored params run

--- tests/kedro_neptune/test_example_project.py
+++ tests/kedro_neptune/test_example_project.py
@@ -27,8 +27,8 @@ def test_standard():
 # def test_parallel_runner():
 #     run_pipeline(project="planets", run_params={"runner": ParallelRunner(2)})
 #     assert_structure()
-#
-#
-# def test_run_with_params():
-#     run_pipeline(project="planets", session_params={"extra_params": {"travel_speed": 40000}})
-#     assert_structure(travel_speed=40000)
+
+
+def test_run_with_params():
+    run_pipeline(project="planets", session_params={"extra_params": {"travel_speed": 40000}})
+    assert_structure(travel_speed=40000)
