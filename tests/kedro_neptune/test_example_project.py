d3413f04b2c13738e5dac31113ff3f33fce1c801
Rafal Jankowski <rafal.jankowski@neptune.ai>
2022-11-24 16:37:46 +0100

    Without parallel

--- tests/kedro_neptune/test_example_project.py
+++ tests/kedro_neptune/test_example_project.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from kedro.runner import ParallelRunner
+# from kedro.runner import ParallelRunner
 
 from tests.kedro_neptune.utils.kedro_utils import run_pipeline
 from tests.kedro_neptune.utils.run_utils import assert_structure
@@ -29,6 +29,7 @@ def test_run_with_params():
     assert_structure(travel_speed=40000)
 
 
-def test_parallel_runner():
-    run_pipeline(project="planets", run_params={"runner": ParallelRunner(2)})
-    assert_structure()
+#
+# def test_parallel_runner():
+#     run_pipeline(project="planets", run_params={"runner": ParallelRunner(2)})
+#     assert_structure()
