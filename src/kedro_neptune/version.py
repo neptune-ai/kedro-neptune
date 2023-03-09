d15c57dbc6a95f7df321b45eb8d04f9b87e44fbe
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-03-09 17:39:23 +0100

    handle case when neptune not installed

--- src/kedro_neptune/version.py
+++ src/kedro_neptune/version.py
@@ -1,6 +1,7 @@
 __all__ = ["__version__"]
 
 import sys
+from importlib.util import find_spec
 
 if sys.version_info >= (3, 8):
     from importlib.metadata import (
@@ -12,6 +13,16 @@ else:
         PackageNotFoundError,
         version,
     )
+
+if not (find_spec("neptune") or find_spec("neptune-client")):
+    msg = """
+            The Neptune client library was not found.
+
+            Install the neptune package with
+                `pip install neptune`
+
+            Need help? -> https://docs.neptune.ai/setup/installation/"""
+    raise PackageNotFoundError(msg)
 
 try:
     __version__ = version("kedro-neptune")
