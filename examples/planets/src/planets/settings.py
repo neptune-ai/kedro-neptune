ac015df77b28aa7af288d5e03c83f81946b34637
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-11-09 17:12:24 +0100

    Updated example project settings

--- examples/planets/src/planets/settings.py
+++ examples/planets/src/planets/settings.py
@@ -32,9 +32,12 @@ from kedro.config import OmegaConfigLoader  # noqa: E402
 CONFIG_LOADER_CLASS = OmegaConfigLoader
 # Keyword arguments to pass to the `CONFIG_LOADER_CLASS` constructor.
 CONFIG_LOADER_ARGS = {
-    # "globals_pattern": "*globals.yml",
-    "credentials_neptune": ["credentials_neptune*"],
-    "neptune": ["neptune*"],
+    "config_patterns": {
+        #   "spark" : ["spark*/"],
+        #   "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
+        "credentials_neptune": ["credentials_neptune*"],
+        "neptune": ["neptune*"],
+    }
 }
 
 # Class that manages the Data Catalog.
