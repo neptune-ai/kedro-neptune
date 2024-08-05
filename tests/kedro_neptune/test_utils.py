246781cbb471a6a5732b031d3752f15e6f6ce561
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2024-08-05 11:58:06 +0200

    chore(tests): Refactor get_kedro_env tests and add order of precedence test

--- tests/kedro_neptune/test_utils.py
+++ tests/kedro_neptune/test_utils.py
@@ -16,7 +16,10 @@
 from unittest import mock
 from unittest.mock import patch
 
-from kedro_neptune.utils import ensure_bool, get_kedro_env
+from kedro_neptune.utils import (
+    ensure_bool,
+    get_kedro_env,
+)
 
 
 class TestUtils:
@@ -40,44 +43,40 @@ class TestUtils:
 
 
 class TestGetKedroEnv:
+    def _test_get_kedro_env(self, config_loader_args: dict, expected: str) -> None:
+        settings = mock.Mock()
+        settings.CONFIG_LOADER_ARGS = config_loader_args
+        assert get_kedro_env(settings) == expected
+
     def test_env_from_commandline(self):
         """Test that the function returns the value of the `--env` commandline argument."""
         with patch("sys.argv", ["kedro", "run", "--env", "test"]):
-            settings = mock.Mock()
-            settings.CONFIG_LOADER_ARGS = {}
-            expected = "test"
-            result = get_kedro_env(settings)
-            assert result == expected
+            self._test_get_kedro_env(config_loader_args={}, expected="test")
 
     def test_env_from_environment_variable(self):
         """Test that the function returns the value of the `$KEDRO_ENV` environment variable."""
         with patch("os.environ", {"KEDRO_ENV": "test"}):
-            settings = mock.Mock()
-            settings.CONFIG_LOADER_ARGS = {}
-            expected = "test"
-            result = get_kedro_env(settings)
-            assert result == expected
+            self._test_get_kedro_env(config_loader_args={}, expected="test")
 
     def test_env_from_default_run_env(self):
         """Test that the function returns the value of `settings.CONFIG_LOADER_ARGS["default_run_env"]`."""
-        settings = mock.Mock()
-        settings.CONFIG_LOADER_ARGS = {"default_run_env": "test"}
-        expected = "test"
-        result = get_kedro_env(settings)
-        assert result == expected
+        self._test_get_kedro_env(config_loader_args={"default_run_env": "test"}, expected="test")
 
     def test_env_from_base_env(self):
         """Test that the function returns the value of `settings.CONFIG_LOADER_ARGS["base_env"]`."""
-        settings = mock.Mock()
-        settings.CONFIG_LOADER_ARGS = {"base_env": "test"}
-        expected = "test"
-        result = get_kedro_env(settings)
-        assert result == expected
+        self._test_get_kedro_env(config_loader_args={"base_env": "test"}, expected="test")
 
     def test_env_none(self):
         """Test that the function returns `None` when none of the above conditions are met."""
-        settings = mock.Mock()
-        settings.CONFIG_LOADER_ARGS = {}
-        expected = None
-        result = get_kedro_env(settings)
-        assert result == expected
+        self._test_get_kedro_env(config_loader_args={}, expected=None)
+
+    def test_order_of_precedence(self):
+        """Test that the environment is read from `sys.argv`, `$KEDRO_ENV`,
+        `settings.CONFIG_LOADER_ARGS["default_run_env"]` or `settings.CONFIG_LOADER_ARGS["base_env"]`,
+        in order of precedence"""
+
+        with patch("sys.argv", ["kedro", "run", "--env", "argv"]), patch("os.environ", {"KEDRO_ENV": "env"}):
+            self._test_get_kedro_env(
+                config_loader_args={"default_run_env": "default", "base_env": "base"},
+                expected="argv",
+            )
